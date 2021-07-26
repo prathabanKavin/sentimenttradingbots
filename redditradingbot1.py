d546f73872d68d23df202600e95e640ac946fbfd
prathabanKavin <kavinprathaban025@gmail.com>
2021-07-26 22:07:33 +0530

    created reddit instance using reddit api

---
+++ redditradingbot1.py
@@ -0,0 +1,11 @@
+import praw
+import config
+
+reddit = praw.Reddit(
+    client_id=config.REDDIT_ID,
+    client_secret=config.REDDIT_SECRET,
+    password=config.REDDIT_PASS,
+    user_agent="USERAGENT",
+    username=config.REDDIT_USER
+)
+print(reddit)
