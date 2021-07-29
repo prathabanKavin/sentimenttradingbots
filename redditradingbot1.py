7b031fc4a2f309cb2ac756f2893e500db6c14bbd
prathabanKavin <kavinprathaban025@gmail.com>
2021-07-30 00:15:56 +0530

    connected to reddit and performed sentiment analysis

--- redditradingbot1.py
+++ redditradingbot1.py
@@ -1,5 +1,6 @@
 import praw
 import config
+from textblob import TextBlob
 
 reddit = praw.Reddit(
     client_id=config.REDDIT_ID,
@@ -8,4 +9,28 @@ reddit = praw.Reddit(
     user_agent="USERAGENT",
     username=config.REDDIT_USER
 )
-print(reddit)
+
+sentimentList = []
+neededSentiments = 300
+
+
+def Average(lst):
+    if len(lst) == 0:
+        return len(lst)
+    else:
+        return sum(lst[-neededSentiments:])/neededSentiments
+
+
+for comment in reddit.subreddit("bitcoinmarkets").stream.comments():
+    redditComment = comment.body
+    blob = TextBlob(redditComment)
+    sent = blob.sentiment
+    print(comment.body)
+    print("Sentiment is " + str(sent.polarity))
+
+    if sent.polarity != 0.0:
+        sentimentList.append(sent)
+        if round(Average(sentimentList)) > 0.5 and len(sentimentList) > neededSentiments:
+            print("BUY")
+        elif round(Average(sentimentList)) < -0.5 and len(sentimentList) > neededSentiments:
+            print("SELL")
